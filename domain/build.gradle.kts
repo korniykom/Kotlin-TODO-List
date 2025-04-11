d3b11dfaed6497ef65c97fb23d8dcc45fe979a61
Maksym Korniiko <korniykom@gmail.com>
2025-04-11 16:06:42 +0300

    fix: cleaned up domain gradle file

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,42 +1,13 @@
 plugins {
-    id("com.android.library")
-    alias(libs.plugins.kotlin.android)
+    kotlin("jvm")
 }
-
-android {
-    namespace = "com.korniykom.domain"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 26
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt") , "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
-
+kotlin {
+    jvmToolchain(21)
+}
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
-    implementation(libs.javax.inject)
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
